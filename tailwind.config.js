bd7e375497362c78c7cda227b2a35796580a95ee
Devazharruk <azharruk66@gmail.com>
2024-08-20 04:42:20 -0700

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,ejs,cjs}"],
+  content: [],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
+
